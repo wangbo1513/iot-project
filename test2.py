83e5bbf91f121696cb8f8bdc98f6af17766bb777
Wangbo <Wangbo@qq.com>
2026-06-13 18:13:34 +0800

    day3: bmi calculator

---
+++ test2.py
@@ -0,0 +1,4 @@
+name = input("请输入你的名字")
+age = int(input("请输入你的年龄"))
+print("你好,",name)
+print("明年你",age+1,"岁")
---
+++ test3.py
@@ -0,0 +1,4 @@
+height = float(input("身高："))
+weight = float(input("体重:"))
+bmi = weight/(height**2)
+print("你的BMI为：",bmi)
